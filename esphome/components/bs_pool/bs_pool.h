ca970e8b361c9a1c1a4e881f192c2ab1b29b6d7f
Tomáš Lipovský <tomas@lipovo.cz>
2024-02-26 10:11:40 +0100

    init release

---
+++ esphome/components/bs_pool/bs_pool.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace bs_pool {
+
+enum FunctionCode : char {
+};
+
+union DataPacket {
+  uint8_t raw[3];
+  struct {
+    uint8_t function_code;
+    uint8_t data_b2;
+    uint8_t data_b3;
+  };
+} __attribute__((packed));
+
+class BSPoolListener {
+ public:
+  virtual void handle_message(DataPacket &message) = 0;
+  virtual const std::vector<FunctionCode> codes_to_poll() = 0;
+};
+
+class BSPool : public uart::UARTDevice, public PollingComponent {
+ public:
+  void update() override;
+  void loop() override;
+
+  void register_listener(BSPoolListener *listener) {
+    this->listeners_.push_back(listener);
+  }
+
+ protected:
+  DataPacket buffer_;
+
+  std::vector<BSPoolListener *> listeners_{};
+};
+
+}  // namespace bs_pool
+}  // namespace esphome
